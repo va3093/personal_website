1d63db07849a8d65539bc2d3d64058141287cfe6
Wilhelm <va3093@gmail.com>
2020-04-07 15:03:59 +0200

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Header from "../components/Header";
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+
+      <p>Hello Next.js</p>
+    </div>
+  );
+}
