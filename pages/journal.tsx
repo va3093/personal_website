1d9de338a11f8db430ff6ba1ccf01578e20f17b0
Wilhelm <va3093@gmail.com>
2022-07-13 08:42:47 +0100

    with monaco

---
+++ pages/journal.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement, useState } from "react";
+import { PageHead } from "components/PageHead/PageHead";
+import styles from "./index.module.css";
+import journalStyles from "./journal.module.css";
+import cn from "classnames";
+import Editor from "@monaco-editor/react";
+
+const thoughts = ["how are you", "are you there"];
+
+export default function Journal(): ReactElement {
+  const [val, setVal] = useState("");
+  return (
+    <>
+      <PageHead title="Wilhelm's site" />
+      <div className={cn(styles.page, journalStyles.page)}>
+        <h1>Journal</h1>
+        <p>{thoughts[new Date().getTime() % thoughts.length]}</p>
+        <div className={journalStyles.textWrapper}>
+          <Editor
+            height="90vh"
+            defaultLanguage="markdown"
+            defaultValue="// some comment"
+            options={{
+              lineNumbers: "off",
+              minimap: { enabled: false },
+              fontSize: 16,
+              wordWrap: "on",
+              overviewRulerBorder: false,
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
