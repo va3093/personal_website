e29fe28240fbe59431afd4afebc0272abd6e561d
Wilhelm <va3093@gmail.com>
2022-07-13 08:58:02 +0100

    simpler version

--- pages/journal.tsx
+++ pages/journal.tsx
@@ -3,9 +3,8 @@ import { PageHead } from "components/PageHead/PageHead";
 import styles from "./index.module.css";
 import journalStyles from "./journal.module.css";
 import cn from "classnames";
-import Editor from "@monaco-editor/react";
 
-const thoughts = ["how are you", "are you there"];
+const thoughts = ["Are you progressing towards your primary goals", "What are your primary goals"];
 
 export default function Journal(): ReactElement {
   return (
@@ -15,18 +14,7 @@ export default function Journal(): ReactElement {
         <h1>Journal</h1>
         <p>{thoughts[new Date().getTime() % thoughts.length]}</p>
         <div className={journalStyles.textWrapper}>
-          <Editor
-            height="90vh"
-            defaultLanguage="markdown"
-            defaultValue="// some comment"
-            options={{
-              lineNumbers: "off",
-              minimap: { enabled: false },
-              fontSize: 16,
-              wordWrap: "on",
-              overviewRulerBorder: false,
-            }}
-          />
+          <textarea />
         </div>
       </div>
     </>
